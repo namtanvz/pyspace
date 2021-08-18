5d8327b84ed594be5c1d95e7bb856d209c43ef49
Nubthong Worathong <nubthongwor@gmail.com>
2021-08-18 21:52:19 +0700

    Update qbits.py

--- fastest/qbits.py
+++ fastest/qbits.py
@@ -8,3 +8,20 @@ elif s == "TB":n*=1024*1024*1024*1024*8
 r = 0
 while 2**r<n:r+=1
 print(r)
+/ ------------------------------------ /
+n = int(input())
+s = input()
+
+c = ["KB","MB","GB","TB"]
+if s == "byte":
+    f = n*8
+else:
+    p = c.index(s)
+    f = n*8*1024**(p+1) 
+
+i = 1
+g = 1
+while g < f:
+    g*=2
+    i+=1
+print(i-1)
